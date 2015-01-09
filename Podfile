d5a1e0d738546e41886c2091bbc27b40b89070e6
Zi-long QIU <zilong.qiu@gmail.com>
2015-01-09 16:02:05 +0100

    Project files

---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.1'
+
+pod 'AFNetworking', '~> 2.0'  
+pod 'ObjectiveSugar', '~> 0.5'
---
+++ UserMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserMap
+//
+//  Created by Zi-long Qiu on 09/01/2015.
+//  Copyright (c) 2015 Zilong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
