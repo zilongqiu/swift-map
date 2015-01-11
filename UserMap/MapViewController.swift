d822ae22222f23f09942ffae097782b85ba8cb6c
Zi-long QIU <zilong.qiu@gmail.com>
2015-01-11 12:57:39 +0100

    Add user location

--- UserMap/MapViewController.swift
+++ UserMap/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  UserMap
 //
 //  Created by Zi-long Qiu on 09/01/2015.
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet var mapView: GMSMapView!
-    @IBOutlet var mapCenterPin: UIImageView!
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         // log
         println("STATUS : APP STARTING");
         
-        super.viewDidLoad()
-
+        mapView.setMinZoom(4.0, maxZoom: 15.0)
         
+        // Add a flag
+        /*
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(41.887, -87.622)
+        marker.appearAnimation = kGMSMarkerAnimationPop
+        marker.icon = UIImage(named: "flag_icon")
+        marker.map = self.mapView
+        */
+        
+        // Ask access to user location
+        self.locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        println("\(self.mapView.myLocation)")
+        if let mylocation = self.mapView.myLocation {
+            NSLog("\(mylocation)")
+        } else {
+            NSLog("User's location is unknown")
+        }
+        
+    }
+    
+    // Invoke CLLocationManagerDelegate when the user grants or revokes location permissions
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        if status == .AuthorizedWhenInUse {
+
+            self.locationManager.startUpdatingLocation()
+ 
+            self.mapView.myLocationEnabled = true
+            self.mapView.settings.myLocationButton = true
+        }
+    }
+    
+    // CLLocationManagerDelegate executes when the location manager receives new location data
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        if let location = locations.first as? CLLocation {
+    
+            self.mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+
+            self.locationManager.stopUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
